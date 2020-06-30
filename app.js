0d09e90dcd998825992fabb4dc250fabea4ff548
Erika Pauwels <erika.pauwels@gmail.com>
2020-06-30 10:04:50 +0200

    Make response JSONAPI compliant

--- app.js
+++ app.js
@@ -1,35 +1,50 @@
 import { app, errorHandler } from 'mu';
 import { sparqlEscapeUri, sparqlEscapeDateTime, query } from 'mu';
 
-const ttlToDeltaUri = 'http://redpencil.data.gift/services/ttl-do-delta-service';
-
 app.get('/files', async function( req, res ) {
   const since = req.query.since || new Date().toISOString();
   const files = await getDeltaFiles(since);
   res.json({ data: files });
 } );
 
-
+/**
+ * Get all delta files produced by the ttl-to-delta service since a given timestamp
+ *
+ * @param since {string} ISO date time
+ * @private
+ */
 async function getDeltaFiles(since) {
-  const queryString = `
-    PREFIX xsd: <http://www.w3.org/2001/XMLSchema#>
-    PREFIX dct: <http://purl.org/dc/terms/>
+  console.log(`Retrieving delta files since ${since}`);
+  const result = await query(`
+    PREFIX mu: <http://mu.semte.ch/vocabularies/core/>
     PREFIX nfo: <http://www.semanticdesktop.org/ontologies/2007/03/22/nfo#>
     PREFIX nie: <http://www.semanticdesktop.org/ontologies/2007/01/19/nie#>
+    PREFIX dct: <http://purl.org/dc/terms/>
+    PREFIX xsd: <http://www.w3.org/2001/XMLSchema#>
 
-    SELECT ?logicalFileUri
-    WHERE
-    {
-      ?physicalFile nie:dataSource ?logicalFileUri.
-      ?logicalFileUri a nfo:FileDataObject;
-        dct:created ?date;
-        dct:creator ${sparqlEscapeUri(ttlToDeltaUri)}.
-      FILTER (?date > ${sparqlEscapeDateTime(since)})
-    }
-    ORDER BY ASC(?date)
-  `;
-  const result = await query(queryString);
-  return result.results.bindings.map((result) => result.logicalFileUri.value);
+    SELECT ?uuid ?filename ?created
+    WHERE {
+      ?s a nfo:FileDataObject ;
+        mu:uuid ?uuid ;
+        nfo:fileName ?filename ;
+        dct:publisher <http://redpencil.data.gift/services/ttl-to-delta-service> ;
+        dct:created ?created .
+      ?file nie:dataSource ?s .
+
+      FILTER (?created > "${since}"^^xsd:dateTime)
+    } ORDER BY ?created
+  `);
+
+  return result.results.bindings.map(b => {
+    return {
+      type: 'files',
+      id: b['uuid'].value,
+      attributes: {
+        name: b['filename'].value,
+        created: b['created'].value
+      }
+    };
+  });
 }
 
 app.use(errorHandler);
